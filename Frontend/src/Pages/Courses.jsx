567646340079b3565d6392f28b656a2959e9e562
harshalhonde21 <harshalhonde17@gmail.com>
2024-02-16 17:01:54 +0530

    updated

--- Frontend/src/Pages/Courses.jsx
+++ Frontend/src/Pages/Courses.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import './Courses.css';
